e1fc15d6e7db84552e3114b5c7f51eb13490e970
MaskerT <1072960055@qq.com>
2022-10-30 14:28:45 +0800

    移除echat

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -30,40 +30,6 @@
 </template>
 
 <script setup>
-// import {onMounted, onUnmounted} from "vue";
-//
-// let myChart = null
-//
-// onMounted(() => {
-//   if (window.echats) {
-//     //  基于准备好的dom节点，初始化echarts实例
-//     myChart = window.echats.init(document.getElementById('zoom'))
-//
-//     //  指定图表的配置项和数据
-//     const option = {
-//       xAxis: {
-//         type: 'category',
-//         data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-//       },
-//       yAxis: {
-//         type: 'value'
-//       },
-//       series: [
-//         {
-//           data: [820, 932, 901, 934, 1290, 1330, 1320],
-//           type: 'line',
-//           smooth: true
-//         }
-//       ]
-//     }
-//
-//     // 使用刚指定的配置项和数据显示图表。
-//     myChart.setOption(option)
-//   }
-//   onUnmounted(() => {
-//     myChart.dispose()
-//   })
-// })
 </script>
 
 <style scoped>
